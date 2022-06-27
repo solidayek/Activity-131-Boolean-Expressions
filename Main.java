d199377dec9d6e76623a879f053bb974a1f7f3ec
solidayek <87338107+solidayek@users.noreply.github.com>
2022-06-27 15:26:05 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+/*
+ * Activity 1.3.1
+ */
+public class Main 
+{
+  public static void main(String[] args) 
+  {
+    int a = 1;
+    int b = 8;
+    int c = 2;
+    int d = 5;
+      
+    /* your code here */
+    System.out.println("a < 5: " + (a < 5));
+    
+    //mycodes
+    System.out.println("b > 8: " + (b > 8));
+    System.out.println("c != 2: " + (c != 2));
+    System.out.println("d <= 8: " + (d <= 8));
+
+
+    
+  }
+}
+
